76242d09c562f63ca8d77eefcf23f40dcc4e22bc
chitodreco <roberto.panizo.ateza@gmail.com>
2026-04-20 20:15:07 +0200

    Update App.java
    añadido hola hola

--- src/main/java/com/campusvirtual/App.java
+++ src/main/java/com/campusvirtual/App.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 /**
  * Punto de entrada de la aplicación Campus Virtual.
  * Inicializa la conexión a BD y muestra la ventana de login.......
+ * hola hola
  */
 public class App extends Application {
 
